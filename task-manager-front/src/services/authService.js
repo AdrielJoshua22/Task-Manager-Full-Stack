ffdef26f72bb07d5fa61c854902f520df6857f35
Adriel Joshua <joshuaamengual9305@gmail.com>
2026-03-15 15:56:15 -0300

    chore: forzar despliegue con urls de railway finales

--- task-manager-front/src/services/authService.js
+++ task-manager-front/src/services/authService.js
@@ -1,10 +1,8 @@
 import axios from 'axios';
 
-// La URL base de tu API de autenticación en Railway
 const API_URL = 'https://task-manager-full-stack-production.up.railway.app/api/auth';
 
 export const login = async (username, password) => {
-  // Aquí es donde se le suma el /login final
   const response = await axios.post(`${API_URL}/login`, { username, password });
   return response.data;
 };
