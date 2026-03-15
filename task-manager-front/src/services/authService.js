88f5eddb9df3d40c2bf0f7c71a1c0c12ad7052da
Adriel Joshua <joshuaamengual9305@gmail.com>
2026-03-15 15:28:15 -0300

    fix: correccion de authservice login2

--- task-manager-front/src/services/authService.js
+++ task-manager-front/src/services/authService.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const API_URL = 'https://task-manager-full-stack-production.up.railway.app/api/auth/login';
+const AUTH_URL = 'https://task-manager-full-stack-production.up.railway.app/api/auth';
 
 export const login = async (username, password) => {
-  const response = await axios.post(`${API_URL}/login`, { username, password });
+  const response = await axios.post(`${AUTH_URL}/login`, { username, password });
   return response.data;
 };
