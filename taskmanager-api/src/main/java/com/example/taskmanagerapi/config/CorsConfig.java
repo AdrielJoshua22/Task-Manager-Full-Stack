ab92564df304a9a843b31bdc4584889c013145ee
Adriel Joshua <joshuaamengual9305@gmail.com>
2026-03-18 12:18:15 -0300

    feat:errores de CORSCONFIGURE.

--- taskmanager-api/src/main/java/com/example/taskmanagerapi/config/CorsConfig.java
+++ taskmanager-api/src/main/java/com/example/taskmanagerapi/config/CorsConfig.java
@@ -16,11 +16,11 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-
         config.setAllowedOriginPatterns(Collections.singletonList("*"));
         config.setAllowedHeaders(Collections.singletonList("*"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
-        config.setAllowCredentials(false);
+        config.setAllowCredentials(true);
+
         config.setMaxAge(3600L);
 
         source.registerCorsConfiguration("/**", config);
